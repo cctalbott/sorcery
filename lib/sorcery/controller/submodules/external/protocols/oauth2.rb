5d19aea8bbf91913472b9994cca23f69de7cbab0
przemad <przemad@gmail.com>
2012-01-10 13:13:44 +0100

    monkey patch for oauth2 v5.1 and sorcery 0.7.6 for facebook

--- lib/sorcery/controller/submodules/external/protocols/oauth2.rb
+++ lib/sorcery/controller/submodules/external/protocols/oauth2.rb
@@ -19,10 +19,17 @@ module Sorcery
             end
 
             def get_access_token(args, options = {})
+              options = options.merge( :token_url => 'oauth/access_token' ) # only for facebook
               client = build_client(options)
-              client.auth_code.get_access_token(
+              client.auth_code.get_token(
                 args[:code],
-                :redirect_uri => @callback_url
+                {
+                  :redirect_uri => @callback_url,
+                  :parse => :query
+                },
+                {
+                  :header_format => 'OAuth %s'
+                }
               )
             end
 
